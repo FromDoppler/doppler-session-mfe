a3ec0f85064acf8b3f95a687bafbb91d7015fdcc
Andrés Moschini <andresmoschini@gmail.com>
2022-04-05 14:41:25 -0300

    chore: add DopplerLegacyClient based on Editors WebApp's implementation

---
+++ src/doppler-legacy-client/abstractions.ts
@@ -0,0 +1,29 @@
+import { Result } from "../common/abstractions";
+
+type DopplerLegacyUserData = {
+  jwtToken: string;
+  user: {
+    email: string;
+    fullname: string;
+    lang: string;
+    avatar: {
+      text: string;
+      color: string;
+    };
+  };
+  unlayerUser: { id: string; signature: string };
+};
+
+type DopplerUserDataNotAvailableError = {
+  userDataNotAvailable: true;
+  innerError: unknown;
+};
+
+export type GetDopplerUserDataResult = Result<
+  DopplerLegacyUserData,
+  DopplerUserDataNotAvailableError
+>;
+
+export interface DopplerLegacyClient {
+  getDopplerUserData: () => Promise<GetDopplerUserDataResult>;
+}
